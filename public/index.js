7722b338799bf5700bb1a481d9b8b017f751a480
Jeremy Gruhlkey <jeremygruhlkey@Naeuns-MacBook-Pro.local>
2018-03-14 21:35:54 -0700

    Connection to IEX returning data
    data returns to server
    server connected to frontend clients
    need to broadcast data to frontend

--- public/index.js
+++ public/index.js
@@ -2,3 +2,4 @@
 // this 'socket' is a separate frontend socket variable
 // and not connected to the one in server on the backend
 var socket = io.connect('http://localhost:3000');
+
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var socket = require('socket.io');
 
+//
+const IEXsocket = require('socket.io-client')('https://ws-api.iextrading.com/1.0/tops')
+//
+
 var app = express();
 
 var PORT = 3000;
@@ -19,5 +23,29 @@ var io = socket(server);
   // which then runs the function callback with the paramater
   // socket referring to the socket between the server and THAT client
 io.on('connection', function(socket){
-    console.log("Made socket connection " + socket.id )
+    console.log("Made socket connection " + socket.id);
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
+
+// Listen to the channel's messages
+IEXsocket.on('message', message => {
+    // for(var key in message){
+        data= JSON.parse(message)
+        console.log("Symbol: " + data.symbol + ", Price: " + data.lastSalePrice)
+    // }
 })
+
+// Connect to the channel
+IEXsocket.on('connect', () => {
+
+  // Subscribe to topics (i.e. appl,fb,aig+)
+  IEXsocket.emit('subscribe', 'snap,fb,aig')
+
+  // Unsubscribe from topics (i.e. aig+)
+//   IEXsocket.emit('unsubscribe', 'aig+')
+})
+
+// Disconnect from the channel
+IEXsocket.on('disconnect', () => console.log('Disconnected.'))
