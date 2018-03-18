e993947d4729fde809a54bc6d9a062bf95a16c12
Jeremy Gruhlkey <jeremygruhlkey@Naeuns-MacBook-Pro.local>
2018-03-17 18:36:04 -0700

    Proof of concept code for user specific sockets
    working
    generates random uuid from faker.js
    and random portfolio from stocks array
    will need to refactor with data from IEX response

--- public/index.js
+++ public/index.js
@@ -1,12 +1,37 @@
-// Make connection
-// this 'socket' is a separate frontend socket variable
-// and not connected to the one in server on the backend
-var socket = io.connect('http://localhost:3000');
+$(document).ready(function(){
 
-socket.on("broadcast", function(data){
-    // console.log(data);
-})
 
-socket.on("portfolio", function(data){
-    console.log(data);
+    // Make connection
+    // this 'socket' is a separate frontend socket variable
+    // and not connected to the one in server on the backend
+    var socket = io('http://localhost:3000');
+
+    var userID = faker.random.uuid
+    var stocks = ['snap', 'aig', 'fb', 'aapl', 'amzn', 'abt', 'ach', 'mdc', 'mfa'];
+    var randomNum = Math.floor(Math.random() * stocks.length);
+    var userStocks = stocks.slice(randomNum);
+
+    var user = {
+        id: userID,
+        stocks: userStocks
+    }
+
+    socket.on("broadcast", function(data){
+        console.log(data);
+        
+    })
+
+    socket.on("connect", function(){
+        socket.emit("loggedin", user); // sends the user id
+
+        socket.on("newData", function(data){
+            // stock data
+            console.log(data);
+        })
+    })
+
+
+
+
+
 })
--- server.js
+++ server.js
@@ -23,6 +23,35 @@ var io = socket(server);
   // socket referring to the socket between the server and THAT client
 io.on('connection', function(socket){
     console.log("Made socket connection " + socket.id);
+
+    var iex = require("socket.io-client")('https://ws-api.iextrading.com/1.0/tops');
+
+    socket.on("loggedin", (user) => {
+        const { id, stocks} = user;
+
+        //connect to the channel
+        iex.on("connect", () => {
+            iex.emit("subscribe", stocks.join(",")) //will be dynamic data from db
+
+            var newData = {
+                stocks,
+                money: stocks.length * 100
+            }
+
+            setInterval(() => {
+                socket.emit("newData", newData)
+            }, 3000);
+        })
+
+        // iex.on("message", message => {
+        //     console.log(message)
+        //     // process data and send back to user on each update from IEX
+        //     socket.emit("newData", 'some stock data here')
+        // })
+
+    })
+
+
     socket.on('disconnect', function(){
         console.log('user disconnected');
     });
@@ -33,7 +62,7 @@ IEXsocket.on('message', message => {
         let data= JSON.parse(message)
         let symbol = data.symbol;
         let lastPrice = data.lastSalePrice;
-        // console.log("Symbol: " + symbol + ", Price: " + lastPrice)
+        console.log("Symbol: " + symbol + ", Price: " + lastPrice)
         io.sockets.emit("broadcast", {description: "price " + lastPrice});
 })
 
@@ -50,23 +79,3 @@ IEXsocket.on('connect', () => {
 // Disconnect from the channel
 IEXsocket.on('disconnect', () => console.log('Disconnected.'))
 
-// issues start here
-app.get("/api/stocks/:stock", function(req, res) {
-    let stock = req.params.stock
-    let userSocket = require('socket.io-client')('https://ws-api.iextrading.com/1.0/tops?symbols=SNAP')
-    // console.log(userSocket);
-    res.json(stock);
-    userSocket.on('response', response => {
-        let data = JSON.parse(response);
-        console.log(data);
-        let symbol = data.symbol;
-        let lastPrice = data.lastSalePrice;
-        console.log("Symbol: " + symbol + ", Price: " + lastPrice)
-        io.sockets.emit("portfolio", {description: "price " + lastPrice});
-    
-    })
-    userSocket.on('connect', () => {
-        console.log("userSocket connected");
-        })
-   
-})
